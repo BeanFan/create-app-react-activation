c353839a877fca1510676111ca2be5974882eea7
BeanFan <fanwanyong_0724@126.com>
2023-04-12 10:33:58 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Routes, Route, BrowserRouter } from "react-router-dom";
+import InputComponent from "./components/TextAreaComponent";
+import TextAreaComponent from "./components/InputComponent";
+import KeepAlive, { useAliveController } from "react-activation";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [inputMorethan10, setInputtext] = useState(false);
+  const inputChange = (text) => {
+    setInputtext(text.length > 10);
+  };
+  const { drop, dropScope, clear, getCachingNodes } = useAliveController();
+  // useEffect(() => {
+  //   if (inputMorethan10) {
+  //     clear(); // seems not work
+  //   } else {
+  //     // refresh("input");
+  //   }
+  // }, [clear, inputMorethan10]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/input">
+            go to keepALive Input when input length is less than 10
+          </Link>
+        </li>
+        <li>
+          <Link to="/textArea">go to Text area</Link>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <Routes>
+            <Route
+              path="/input"
+              element={
+                <KeepAlive
+                  cacheKey={"UNIQUE_ID_INPUT"}
+                  when={() => !inputMorethan10}
+                >
+                  <InputComponent inputChange={inputChange} />
+                </KeepAlive>
+              }
+            ></Route>
+            <Route path="/textArea" element={<TextAreaComponent />}></Route>
+          </Routes>
+        </li>
+      </ul>
+    </BrowserRouter>
   );
 }
 
